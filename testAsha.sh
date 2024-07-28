fdc01b3c6fc911527d94deaa276b41598bad7e86
Utpal Sarkar <utpsark@gmail.com>
2024-07-28 15:19:00 -0700

    How git works

---
+++ testAsha.sh
@@ -0,0 +1,8 @@
+if [ "$seconds" -eq 0 ];then
+   $timezone_string="Z"
+elif[ "$seconds" -gt 0 ]
+then
+   $timezone_string=`printf "%02d:%02d" $seconds/3600 ($seconds/60)%60`
+else
+   echo "Unknown parameter"
+fi
